efb2aa64c72babfd4be83decb72fc31675e6bd57
Gabriel Margoses <gabrielmargoses@gmail.com>
2019-03-10 12:28:22 +0100

    creating types.js

---
+++ client/src/actions/types.js
@@ -0,0 +1,10 @@
+export const GET_CARS = "GET_CARS";
+export const ADD_CAR = "ADD_CAR";
+export const DELETE_CAR = "DELETE_CAR";
+export const UPDATE_CAR = "UPDATE_CAR";
+export const CARS_LOADING = "CARS_LOADING";
+
+export const GET_STATIONS = "GET_STATIONS";
+export const ADD_STATION = "ADD_STATION";
+export const DELETE_STATION = "DELETE_STATION";
+export const STATIONS_LOADING = "STATIONS_LOADING";
